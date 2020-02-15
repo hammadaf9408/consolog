fa6a273775ae5bc0e6af3fb7537cd04a16978244
Muhammad Afif <hammadaf9408@gmail.com>
2020-02-15 12:46:34 +0700

    Add skeleton while loading

--- src/components/dashboard/leftContainer/menu/Menu.tsx
+++ src/components/dashboard/leftContainer/menu/Menu.tsx
@@ -1,23 +1,32 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { useStyle } from 'useStyle';
 import { Fab } from '@material-ui/core';
 import AddIcon from '@material-ui/icons/Add';
 import RefreshIcon from '@material-ui/icons/Refresh';
 import ChatIcon from '@material-ui/icons/Chat';
 import SettingsIcon from '@material-ui/icons/Settings';
+import { LoadingContext } from 'context/loading/loadingContext';
 
 export const Menu: React.FC<any> = props => {
   const classes = useStyle();
-
+  const loadingContext = useContext(LoadingContext);
+  const { loading, setLoading, resetLoading } = loadingContext;
+  
   return (
     <div style={{ display: 'flex', flexDirection: 'column'}}>
-      <Fab color="primary" aria-label="add" className={classes.fabIcon}>
+      <Fab color="primary" aria-label="add" className={classes.fabIcon} disabled={loading}>
         <AddIcon />
       </Fab>
-      <Fab color="primary" aria-label="add" className={classes.fabIcon}>
+      <Fab color="primary" aria-label="add" className={classes.fabIcon} onClick={() => {
+        if (loading) {
+          resetLoading()
+        } else {
+          setLoading()
+        }
+      }}>
         <RefreshIcon />
       </Fab>
-      <Fab color="primary" aria-label="add" className={classes.fabIcon}>
+      <Fab color="primary" aria-label="add" className={classes.fabIcon} disabled={loading}>
         <ChatIcon />
       </Fab>
       <div style={{margin: '0 auto'}}>
--- src/components/dashboard/leftContainer/note/NoteList.tsx
+++ src/components/dashboard/leftContainer/note/NoteList.tsx
@@ -1,36 +1,66 @@
-import React from "react";
+import React, { useContext } from "react";
 import {
   Divider,
   List,
   ListItem,
   ListItemText,
   ListItemSecondaryAction,
-  Typography,
+  Typography
 } from "@material-ui/core";
 import moment from 'moment';
 import LockIcon from "@material-ui/icons/LockOutlined";
 import LockOpenIcon from "@material-ui/icons/LockOpenOutlined";
 import { useStyle } from "useStyle";
+import Skeleton from '@material-ui/lab/Skeleton';
+import { LoadingContext } from "context/loading/loadingContext";
 
 export const NoteList: React.FC<any> = props => {
   const classes = useStyle();
+  const loadingContext = useContext(LoadingContext);
+  const { loading } = loadingContext;
+
   const dummy: String[] = ['Inbox', 'Starred', 'Send email', 'Drafts', 'Inbox', 'Starred', 'Send email', 'Drafts', 'Inbox', 'Starred', 'Send email', 'Drafts Drafts Drafts Drafts Drafts Drafts Drafts Drafts'];
   
-  return (
-    <List>  
-      {dummy.map((text, index) => (
-        <React.Fragment key={index}>
-          <ListItem button>
+  const skeleton = () => {
+    const render = [];
+
+    for (let i = 0; i < 9; i++) {
+      render.push(
+        <React.Fragment key={i}>
+          <ListItem>
             <ListItemText 
-              primary={<Typography className={classes.textElipsis}>{text}</Typography>} 
-              secondary={moment().format('MMM D, YYYY')} />
-            <ListItemSecondaryAction>
-              {index % 2 === 0 ? <LockIcon color="disabled" /> : <LockOpenIcon color="disabled" />}
-            </ListItemSecondaryAction>
+              primary={<Skeleton className={classes.skeleton} variant="text" width={`${Math.floor((Math.random() * 40) + 40)}%`} />}
+              secondary={<Skeleton className={classes.skeleton} width={'30%'} />}
+            />
           </ListItem>
           <Divider />
         </React.Fragment>
-      ))}
+      )
+    }
+
+    return render;
+  }
+
+  return (
+    <List>
+      {
+        loading ?
+          skeleton()
+        :
+          dummy.map((text, index) => (
+            <React.Fragment key={index}>
+              <ListItem button>
+                <ListItemText 
+                  primary={<Typography className={classes.textElipsis}>{text}</Typography>} 
+                  secondary={moment().format('MMM D, YYYY')} />
+                <ListItemSecondaryAction>
+                  {index % 2 === 0 ? <LockIcon color="disabled" /> : <LockOpenIcon color="disabled" />}
+                </ListItemSecondaryAction>
+              </ListItem>
+              <Divider />
+            </React.Fragment>
+          ))
+      }
     </List>
   );
 };
