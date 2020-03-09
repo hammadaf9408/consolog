68668aa3ab91fad1a7c4f5c73db2be3e893737b3
Muhammad Afif <hammadaf9408@gmail.com>
2020-03-09 15:10:35 +0700

    Icon for duedate on list

--- src/components/dashboard/leftContainer/note/NoteList.tsx
+++ src/components/dashboard/leftContainer/note/NoteList.tsx
@@ -7,11 +7,13 @@ import {
   ListItemSecondaryAction,
   Typography,
   WithStyles, 
-  withStyles 
+  withStyles, 
+  Tooltip
 } from "@material-ui/core";
 import moment from 'moment';
-import LockIcon from "@material-ui/icons/LockOutlined";
-import LockOpenIcon from "@material-ui/icons/LockOpenOutlined";
+import AssignmentIcon from "@material-ui/icons/AssignmentOutlined";
+import AssignmentOkIcon from "@material-ui/icons/AssignmentTurnedInOutlined";
+import AssignmentLateIcon from "@material-ui/icons/AssignmentLateOutlined";
 import { LoadingContext } from "context/loading/loadingContext";
 import { SkeletonLoad } from "../skeleton/SkeletonLoad";
 import { NotesContext } from "components/dashboard/context/notes/notesContext";
@@ -37,7 +39,7 @@ const NoteListView: React.FC<AllProps> = props => {
   const loadingContext = React.useContext(LoadingContext);
   const notesContext = React.useContext(NotesContext);
 
-  const { loadAllNotes, allNotes, loadSingleNote } = notesContext;
+  const { loadAllNotes, allNotes, loadSingleNote, singleNote } = notesContext;
   const { loading } = loadingContext;
 
   /* ============================================ USEEFFECT =========================================== */
@@ -60,12 +62,32 @@ const NoteListView: React.FC<AllProps> = props => {
         :
           allNotes?.data?.map((item, index) => (
             <React.Fragment key={item._id}>
-              <ListItem button onClick={() => loadSingleNote(item)}>
+              <ListItem button onClick={() => loadSingleNote(item)} selected={singleNote?._id === item._id}>
                 <ListItemText 
                   primary={<Typography className={classes.textElipsis}>{item.title}</Typography>} 
                   secondary={moment(item.createdAt).format('MMM D, YYYY')} />
                 <ListItemSecondaryAction>
-                  {index % 2 === 0 ? <LockIcon color="disabled" /> : <LockOpenIcon color="disabled" />}
+                  {
+                    item.dueDate &&
+                    item.dueDate.date &&
+                    (
+                      item.dueDate.value ?
+                      <Tooltip title={moment(item.dueDate.date).format('MMM D, YYYY')}>
+                        <AssignmentOkIcon style={{color: '#388e3c'}} />
+                      </Tooltip>
+                      :
+                      (
+                        moment(item.dueDate.date).isBefore(moment()) ?
+                        <Tooltip title={moment(item.dueDate.date).format('MMM D, YYYY')}>
+                          <AssignmentLateIcon style={{color: 'rgba(244, 67, 54, 0.8)'}} />
+                        </Tooltip>
+                        :
+                        <Tooltip title={moment(item.dueDate.date).format('MMM D, YYYY')}>
+                          <AssignmentIcon style={{color: 'rgba(63, 81, 181, 0.8)'}} />
+                        </Tooltip>
+                      )
+                    )
+                  }
                 </ListItemSecondaryAction>
               </ListItem>
               <Divider />
